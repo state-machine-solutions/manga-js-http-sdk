94cf4fb605e5c566d3b5cd82e27fc77f297b4b7b
Renato <reytuty@gmail.com>
2024-01-10 21:31:02 -0300

    chore: prepared to use garbage collector

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ const httpManga = new HttpManga("http://localhost:80");
 
 ```
 
-## SET 
+## SET
 
 ```
 httpManga.set("your.path.here", { yourValueHere: true, anything:[1,2,3]}).then((r)=>{
@@ -21,7 +21,19 @@ httpManga.set("your.path.here", { yourValueHere: true, anything:[1,2,3]}).then((
 });
 ```
 
-## GET 
+## SET with timeout and garbage collector
+
+It just works if the server is setted to use garbage collector `USE_TEMP_DATA=true` on server env
+
+```
+const timeoutInSecconds = 3;
+//after 3 secconds server execute delete to your.path.here
+httpManga.set("your.path.here", { yourValueHere: true, anything:[1,2,3]}, timeoutInSecconds).then((r)=>{
+    console.log(r);
+});
+```
+
+## GET
 
 ```
 httpManga.get("your.path.here").then((r)=>{
@@ -33,6 +45,18 @@ httpManga.get("your.path.here").then((r)=>{
 
 ```
 httpManga.reset("your.path.here", { yourValueHere: true, anything:[1,2,3]}).then((r)=>{
+    console.log(r);
+});
+```
+
+## RESET with timeout and garbage collecotr
+
+It just works if the server is setted to use garbage collector `USE_TEMP_DATA=true` on server env
+
+```
+const timeoutInSecconds = 3;
+//after 3 secconds server execute delete to your.path.here
+httpManga.reset("your.path.here", { yourValueHere: true, anything:[1,2,3]}, timeoutInSecconds ).then((r)=>{
     console.log(r);
 });
 ```
@@ -52,4 +76,3 @@ httpManga.clear().then((r)=>{
     console.log(r);//there is no more data on server
 });
 ```
-
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,82 @@
 import axios from "axios";
 
-export interface MangaPathBody{
-    path?:string;
+export interface MangaPathBody {
+  path?: string;
 }
-export interface MangaValueBody extends MangaPathBody{
-    value:any;
+export interface MangaValueBody extends MangaPathBody {
+  value: any;
+  timeout?: number;
 }
 
 export interface MangaDataReturn {
-    success:boolean;
-    result?:any;
-    messages?:string[];
+  success: boolean;
+  result?: any;
+  messages?: string[];
 }
-export class HttpManga{
-    endpoint: string;
-    requestConfig: { headers: { 'Content-Type': string; Accept: string; }; };
+export class HttpManga {
+  endpoint: string;
+  requestConfig: { headers: { "Content-Type": string; Accept: string } };
 
-    constructor(endpoint:string){
-        this.endpoint = endpoint[endpoint.length-1] == '/'? endpoint.substring(0, endpoint.length-2):  endpoint;
-        this.requestConfig = {
-            headers:{
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            }
-        }
+  constructor(endpoint: string) {
+    this.endpoint =
+      endpoint[endpoint.length - 1] == "/"
+        ? endpoint.substring(0, endpoint.length - 2)
+        : endpoint;
+    this.requestConfig = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+  }
+  protected async post(
+    method: string,
+    body: MangaPathBody | MangaValueBody
+  ): Promise<MangaDataReturn> {
+    const result = await axios.post(
+      this.endpoint + method,
+      body,
+      this.requestConfig
+    );
+    return result.data as MangaDataReturn;
+  }
+  protected async put(
+    method: string,
+    body: MangaValueBody
+  ): Promise<MangaDataReturn> {
+    const result = await axios.put(
+      this.endpoint + method,
+      body,
+      this.requestConfig
+    );
+    return result.data as MangaDataReturn;
+  }
+  public async get(path: string = ""): Promise<any> {
+    let url: string = this.endpoint + "/get";
+    if (path) {
+      url += "?path=" + path;
     }
-    protected async post(method:string, body:MangaPathBody|MangaValueBody):Promise<MangaDataReturn>{
-        const result = await axios.post(this.endpoint+method, body, this.requestConfig);
-        return result.data as MangaDataReturn;
-    }
-    protected async put(method:string, body:MangaValueBody):Promise<MangaDataReturn>{
-        const result = await axios.put(this.endpoint+method, body, this.requestConfig);
-        return result.data as MangaDataReturn;
-    }
-    public async get(path:string = ''):Promise<any>{
-        let url:string = this.endpoint+'/get';
-        if(path){
-            url += '?path='+path;
-        }
-        const result = await axios.get(url, this.requestConfig);
-        return result?.data;
-    }
-    public async set(path:string, value:any):Promise<MangaDataReturn>{
-        return this.post('/set', {path, value})
-    }
-    public async reset(path:string, value:any):Promise<MangaDataReturn>{
-        return this.put('/', {path, value})
-    }
-    public async delete(path:string):Promise<MangaDataReturn>{
-        return this.post('/delete', {path})
-    }
-    public async clear():Promise<MangaDataReturn>{
-        return this.post('/clear', {});
-    }
+    const result = await axios.get(url, this.requestConfig);
+    return result?.data;
+  }
+  public async set(
+    path: string,
+    value: any,
+    timeout?: number
+  ): Promise<MangaDataReturn> {
+    return this.post("/set", { path, value, timeout });
+  }
+  public async reset(
+    path: string,
+    value: any,
+    timeout?: number
+  ): Promise<MangaDataReturn> {
+    return this.put("/", { path, value, timeout });
+  }
+  public async delete(path: string): Promise<MangaDataReturn> {
+    return this.post("/delete", { path });
+  }
+  public async clear(): Promise<MangaDataReturn> {
+    return this.post("/clear", {});
+  }
 }
